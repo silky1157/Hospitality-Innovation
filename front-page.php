cc6baa5ad8b18e1f2cd92cece4249c1e32c261a2
silky1157 <silkymishra1157@gmail.com>
2024-06-14 14:27:53 +0530

    x

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@ get_header(); ?>
 <div class="page-bg">
     <!-- Hero Section -->
     <?php get_template_part("template-parts/front-page/hero-section"); ?>
+    <?php get_template_part("template-parts/front-page/speaker"); ?>
 
 </div>
 
---
+++ template-parts/front-page/speaker.php
@@ -0,0 +1,46 @@
+<section class="section">
+
+
+
+
+<div class="speakers-section">
+  <h2 class="speakers-title">SPEAKERS</h2>
+  <div class="speaker-profiles">
+ 
+    <div class="speaker-info">
+      <img src="dummy_speaker_image_url.jpg" alt="Speaker Photo">
+      <div class="speaker-name">AMR YOUNES</div>
+      <div class="speaker-title">Head of Design Construction - KABC</div>
+    </div>
+    
+    <div class="speaker-info">
+      <img src="dummy_speaker_image_url.jpg" alt="Speaker Photo">
+      <div class="speaker-name">AMR YOUNES</div>
+      <div class="speaker-title">Head of Design Construction - KABC</div>
+    </div>
+    
+    <div class="speaker-info">
+      <img src="dummy_speaker_image_url.jpg" alt="Speaker Photo">
+      <div class="speaker-name">AMR YOUNES</div>
+      <div class="speaker-title">Head of Design Construction - KABC</div>
+    </div>
+    
+    <div class="speaker-info">
+      <img src="dummy_speaker_image_url.jpg" alt="Speaker Photo">
+      <div class="speaker-name">AMR YOUNES</div>
+      <div class="speaker-title">Head of Design Construction - KABC</div>
+    </div>
+   
+    <div class="speaker-info">
+      <img src="dummy_speaker_image_url.jpg" alt="Speaker Photo">
+      <div class="speaker-name">AMR YOUNES</div>
+      <div class="speaker-title">Head of Design Construction - KABC</div>
+    </div>
+   
+  </div>
+  <a href="#" class="view-all-btn">VIEW ALL</a>
+</div>
+
+
+
+</section>
